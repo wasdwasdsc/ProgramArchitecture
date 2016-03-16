ff756164158f8a94d5a9827df72b6ec5c1c5bf57
Dmitriy <dmitriy_gnedoj@mail.ru>
2016-03-17 01:04:30 +0200

    ...

--- model/models.py
+++ model/models.py
@@ -1,3 +1,3 @@
 petrol_price = {"Ai92": 19.5, "Ai95": 20.8, "Ai98": 22.3}
 charge_of_gasoline = {"car1": 6.5, "car2": 9, "car3": 17}
-expense_accounting = {"some_date_1": {"car": 34543}, "some_date_2": {"car": 34543}}
+expense_accounting = {}
